e905ec81134620bf25bfe2cc5043768e4fd624e2
Miguel Ramos <miguel921433@gmail.com>
2023-12-20 20:45:18 -0500

    utlizacion de componentes y slots

--- resources/views/landing/service.blade.php
+++ resources/views/landing/service.blade.php
@@ -1,7 +1,18 @@
 @extends('layout.landing')
-
 @section('title','Service')
 
 @section('content')
-<h1>Service</h1>
+<h1>Services</h1>
+@component('_components.card')
+@slot('title','Service 1')
+@slot('content','lorem ip sum more die')
+@endcomponent
+@component('_components.card')
+@slot('title','Service 2')
+@slot('content','lorem ip sum more die')
+@endcomponent
+@component('_components.card')
+@slot('title','Service 3')
+@slot('content','lorem ip sum more die')
+@endcomponent
 @endsection
--- resources/views/layout/_partials/menu.blade.php
+++ resources/views/layout/_partials/menu.blade.php
@@ -1,10 +1,13 @@
-<header>
-    <nav>
-        <ul>
-            <li><a href="{{route('index')}}">Inicio</a></li>
-            <li><a href="{{route('service')}}">Servicios</a></li>
-            <li><a href="{{route('contact')}}">Contacto</a></li>
-            <li><a href="{{route('about')}}">Acerca de</a></li>
+<header class="bg-gray-800 text-white">
+    <nav class="flex items-center justify-between p-4">
+        <div>
+            <!-- Puedes agregar un logotipo aquí si lo deseas -->
+        </div>
+        <ul class="flex space-x-4">
+            <li><a href="{{ route('index') }}" class="hover:text-gray-300">Inicio</a></li>
+            <li><a href="{{ route('service') }}" class="hover:text-gray-300">Servicios</a></li>
+            <li><a href="{{ route('contact') }}" class="hover:text-gray-300">Contacto</a></li>
+            <li><a href="{{ route('about') }}" class="hover:text-gray-300">Acerca de</a></li>
         </ul>
     </nav>
 </header>
