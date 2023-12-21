d27e8c442113a74aafed4b10e35ece9b320319a0
Miguel Ramos <miguel921433@gmail.com>
2023-12-20 21:31:37 -0500

    clase terminada

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,9 +1,9 @@
-<div  style="border: 1px solid black; border-radius:5px; padding:10px; margin:5px ">
+<div class="card">
     <div class="md:flex">
-        <div class="p-8">
-            <h3 class="text-2xl font-semibold text-gray-800">{{$title}}</h3>
-            <img src="{{ asset('assets/img/nestor.jpg') }}" alt="" width="150">
-            <p class="mt-4 text-gray-600">{{$content}}</p>
-        </div>
+      <div class="p-8">
+        <h1 class="text-2xl font-semibold text-gray-800">Título 1</h1>
+        <img src="{{ asset('assets/img/nestor.jpg') }}" alt="" width="150">
+        <p class="mt-4 text-gray-600">Contenido 1</p>
+      </div>
     </div>
-</div>
+  </div>
--- resources/views/layout/landing.blade.php
+++ resources/views/layout/landing.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>@yield('title')</title>
+    
 </head>
 @include('layout._partials.menu')
 <body>
